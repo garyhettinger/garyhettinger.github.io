5cfcedfcb03133491682c68266e844b28cc0fb35
garyhettinger <ghetting@pennmedicine.upenn.edu>
2024-06-23 18:12:53 -0400

    map updates

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,82 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Boston, Massachusetts",
+    42.3554334,
+    -71.060511
+  ],
+  [
+    "Seattle, Washington",
+    47.6038321,
+    -122.330062
+  ],
+  [
+    "Philadelphia, Pennsylvania",
+    39.9527237,
+    -75.1635262
+  ],
+  [
+    "Atlanta, Georgia",
+    33.7489924,
+    -84.3902644
+  ],
+  [
+    "Lisbon, Portugal",
+    38.7077507,
+    -9.1365919
+  ],
+  [
+    "Baltimore, Maryland",
+    39.2908816,
+    -76.610759
+  ],
+  [
+    "Toronto, Canada",
+    43.6534817,
+    -79.3839347
   ],
   [
     "London, UK",
-    51.5073219,
-    -0.1276473
+    51.4893335,
+    -0.14405508452768728
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Austin, Texas",
+    30.2711286,
+    -97.7436995
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Scottsdale, Arizona",
+    33.4942189,
+    -111.926018
+  ],
+  [
+    "Copenhagen, Denmark",
+    55.6867243,
+    12.5700724
+  ],
+  [
+    "Portland, Oregon",
+    45.5202471,
+    -122.674194
+  ],
+  [
+    "Berkeley, California",
+    37.8708393,
+    -122.272863
+  ],
+  [
+    "Houston, Texas",
+    29.7589382,
+    -95.3676974
+  ],
+  [
+    "Washington D.C.",
+    38.8950368,
+    -77.0365427
+  ],
+  [
+    "Nashville, Tennessee",
+    36.1622767,
+    -86.7742984
   ]
 ];
